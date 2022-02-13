4e532dfd47b39bee3b0daf9e51ba1ca26bbf889d
Cody Canning <codycanning@codys-mbp.lan>
2022-02-13 11:43:41 -0700

    prototype with mock data

--- src/osg/mock.clj
+++ src/osg/mock.clj
@@ -17,16 +17,14 @@
 
 (def titles
   (shuffle
-   ["The sky was clear -- remarkably clear -- and the twinkling of all the stars seemed to be but throbs of one body, timed by a common pulse."
-    "How many goodly creatures are there here! How beauteous mankind is! O brave new world"
-    "When you have done the rashest thing in the world it is very pleasant to be told that no man of spirit could have acted otherwise"
+   ["The sky was clear"
+    "How many goodly creatures are there here!"
+    "When you have done the rashest thing in the world"
     "Love is a smoke rais'd with the fume of sighs"
-    "Great sorrow or great joy should bring intense hunger--not abstinence from food, as our novelists will have it."
-    "Your voice and music are the same to me."
+    "Great sorrow or great joy should bring intense hunger"
+    "Your voice and music are the same to me"
     "Anger is the ultimate destroyer of your own peace of mind"
-    "Appreciate those early influences and what they've done for you"
-    "Make the decision, make another. Remake one past, you cannot"]
-   ))
+    "Make the decision, make another. Remake one past, you cannot"]))
 
 (def txt
   "He an thing rapid these after going drawn or. Timed she his law the spoil round defer. In surprise concerns informed betrayed he learning is ye. Ignorant formerly so ye blessing. He as spoke avoid given downs money on we. Of properly carriage shutters ye as wandered up repeated moreover. Inquietude attachment if ye an solicitude to. Remaining so continued concealed as knowledge happiness. Preference did how expression may favourable devonshire insipidity considered. An length design regret an hardly barton mr figure.")
--- src/osg/site.clj
+++ src/osg/site.clj
@@ -3,9 +3,9 @@
   (:require [clojure.string :as str]
             [hiccup.core :as hiccup]
             [osg.mock :refer [covers titles txt]]
-            [osg.util :refer [layout]]))
+            [osg.util :as util]))
 
-(def tiled-posts
+#_(def tiled-posts
   [:div {:class "tile is-ancestor"}
    [:div {:class "tile is-12"}
     [:div {:class "tile is-parent has-background-grey-dark"}
@@ -31,10 +31,51 @@
         [:img {:class "cropped"
                :src (nth covers 2)}]]]]]]])
 
-(def body
-  [:div.section
-   [:div.container
-    tiled-posts]])
+(def top-hero
+  [:section {:class "hero is-small is-link has-background-grey-lighter has-text-grey-darker"}
+   [:div {:class "hero-body"}
+    [:div {:class "container has-text-centered pt-5 pb-5"}
+     [:p.title {:class "has-text-grey-darker is-italic"} "Okay Sounds Good"]
+     #_ [:p.subtitle "Subtitle"]
+     [:div {:class "pt-3"}
+      [:nav
+       [:a {:class "link is-info mr-5"}
+        "BLOG"]
+       [:a {:class "link is-info"}
+        "ABOUT"]]]]]])
+
+(def footer
+  [:footer {:class "footer has-background-grey-lighter"}])
+
+(defn render-article
+  [idx]
+  [:div {:class "tile is-parent p-2 mb-6"}
+   [:div {:class "tile is-child is-4"}
+    [:figure {:class "image is-3by2"}
+     [:img {:class "cropped"
+            :src (nth covers idx)}]]]
+   [:div {:class "tile is-parent is-vertical is-8 ml-2"}
+    [:div {:class "tile is-child is-uppercase"}
+     [:p {:class "title is-size-5 has-text-grey-dark"} (nth titles idx)]
+     [:p {:class "subtitle is-size-6 has-text-grey"} "BY: Prosciutto"]]
+    [:div {:class "tile is-child"}
+     [:p {:class "has-text-grey"}
+      (str (str/join (take (+ 150 (rand-int 30)) txt))  "...")]]]])
+
+(def articles
+  [:section {:class "container is-fluid"}
+   [:div {:class "tile is-ancestor"}
+    [:div {:class "tile is-2"}]
+    (into [:div {:class "tile is-vertical is-8 mt-6"}]
+          (mapv render-article (range 3)))
+    [:div {:class "tile is-2"}]]])
+
 
 (defn get-pages []
-  (merge {"/index.html" (-> body layout hiccup/html)}))
+  {"/index.html"
+   (hiccup/html
+    [:html
+     (util/header)
+     top-hero
+     articles
+     footer])})
